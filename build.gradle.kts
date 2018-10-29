5d3ad642b8a60e83819cbd993b7a38f5ae717b5e
sorin <sorin.irimies@moovel.com>
2018-10-29 20:10:15 +0100

    update to kotlni 1.3, ktor 1.0 and coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktor_version = "0.9.5"
+val ktor_version = "1.0.0-beta-3"
 val exposed_version = "0.10.5"
 val h2_version = "1.4.196"
 
@@ -11,7 +11,7 @@ version = "0.0.1"
 
 plugins {
     application
-    kotlin("jvm") version "1.2.71"
+    kotlin("jvm") version "1.3.0"
     id("com.github.johnrengelman.shadow") version "2.0.1"
 }
 application {
